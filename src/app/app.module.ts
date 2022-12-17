21fa25e9d73a03bc37ccd001f74f368d3cde93e0
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-12-17 16:07:01 +0100

    aggiunti componenti navbar e footer + html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingComponent } from './app-routing.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppRoutingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
