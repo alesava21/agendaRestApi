834cfc0785a8918a8f40584e53fe84e00596b7c4
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-12-17 15:50:55 +0100

    creazione progetto + aggiunta prima pagina welcome

---
+++ src/app/app-routing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-routing',
+  templateUrl: './app-routing.component.html',
+  styleUrls: ['./app-routing.component.css']
+})
+export class AppRoutingComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingComponent } from './app-routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRoutingComponent
   ],
   imports: [
     BrowserModule
